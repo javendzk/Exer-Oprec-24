60f8dbd63cdccc1ceb125330233ecc0a9bfd9950
Javen <javana.dz@gmail.com>
2024-01-26 21:59:45 +0700

    Deploy Retry 7

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Exer-Oprec-24/",
+  plugins: [react()],
 })
