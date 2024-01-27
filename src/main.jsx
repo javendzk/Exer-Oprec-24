11c055498f4d4d09b3605a6abb712b573de72a89
Javen <javana.dz@gmail.com>
2024-01-27 11:03:33 +0700

    Fixed router issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Navigate, Routes } from "react-router-dom"
+import { HashRouter, Route, Navigate, Routes } from "react-router-dom"
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import './index.css'
@@ -9,13 +9,13 @@ import TemperaturePage from './pages/TemperaturePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/currency" element={<CurrencyPage />} />
                 <Route path="/temperature" element={<TemperaturePage />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
-        </Router>
+        </HashRouter>
     </React.StrictMode>
 )
